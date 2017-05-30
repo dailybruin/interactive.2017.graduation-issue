92a2309b3e296cc8a5bb6354e2ea4dada1056785
Zhang Hongyi <zhang.hongyi@outlook.com>
2017-05-30 14:17:20 -0700

    autocomplete locations, working

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 $(() => {
   var $main = $(".main");
   var $qnaModal = $("#qna-modal");
+  var places = algoliasearch.initPlaces();
 
   $(document).foundation();
 
@@ -13,4 +14,43 @@ $(() => {
   })
 
   $qnaModal.foundation("open");
+
+  places
+    .search({"query": "Paris", "type": "city"}, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      console.log(res);
+    });
+
+  var $fromloc = $("#fromloc");
+
+  $("#fromloc").textcomplete([
+    {
+      match: /(^|\b)([\w,\s]{2,})$/,
+      search: function(query, callback) {
+        console.log(query);
+        places.search({
+          "query": query,
+          "type": "city"
+        },
+        (err, res) => {
+          if(err) {
+            throw err;
+          }
+          callback(res.hits);
+        })
+      },
+      // #5 - Template used to display each result obtained by the Algolia API
+      template: function (hit) {
+        // Returns the highlighted version of the name attribute
+        return hit._highlightResult.locale_names.default[0].value;
+      },
+      // #6 - Template used to display the selected result in the textarea
+      replace: function (hit) {
+        return hit.locale_names.default[0];
+      }
+    }
+  ]);
+
 });
